f6971f0d689f384e83d243bc27cbdfbe691fb7f0
Joshua Vial <joshua@enspiral.com>
2014-07-30 23:42:56 +1000

    setting up grunt replace:travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,14 @@
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
+var replace_files = [{
+  expand: true,
+  flatten: true,
+  src: ['./config/config.coffee'],
+  dest: '<%= yeoman.app %>/scripts/'
+}]
+
+
 module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
@@ -332,12 +340,7 @@ module.exports = function (grunt) {
             json: grunt.file.readJSON('./config/environments/development.json')
           }]
         },
-        files: [{
-          expand: true,
-          flatten: true,
-          src: ['./config/config.coffee'],
-          dest: '<%= yeoman.app %>/scripts/'
-        }]
+        files: replace_files
       },
       test: {
         options: {
@@ -345,12 +348,15 @@ module.exports = function (grunt) {
             json: grunt.file.readJSON('./config/environments/test.json')
           }]
         },
-        files: [{
-          expand: true,
-          flatten: true,
-          src: ['./config/config.coffee'],
-          dest: '<%= yeoman.app %>/scripts/'
-        }]
+        files: replace_files 
+      },
+      travis: {
+        options: {
+          patterns: [{
+            json: grunt.file.readJSON('./config/environments/travis.json')
+          }]
+        },
+        files: replace_files 
       },
       staging: {
         options: {
@@ -358,12 +364,7 @@ module.exports = function (grunt) {
             json: grunt.file.readJSON('./config/environments/staging.json')
           }]
         },
-        files: [{
-          expand: true,
-          flatten: true,
-          src: ['./config/config.coffee'],
-          dest: '<%= yeoman.app %>/scripts/'
-        }]
+        files: replace_files
       },
       production: {
         options: {
@@ -371,12 +372,7 @@ module.exports = function (grunt) {
             json: grunt.file.readJSON('./config/environments/production.json')
           }]
         },
-        files: [{
-          expand: true,
-          flatten: true,
-          src: ['./config/config.coffee'],
-          dest: '<%= yeoman.app %>/scripts/'
-        }]
+        files: replace_files
       }
     },
     protractor: {
